eab7eb46b913b912877d6dde8f5785c4c6812226
Matthew Springer <Mspringer97@gmail.com>
2024-09-13 10:44:13 -0500

    We're gonna see how this goes

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
+import sys
+import pygame
+from pygame.locals import *
+import gDraggable
+import gScreen
+import gChessBoard
 
-def doAdd(x, y):
-    return x + y
+test = gScreen.gameScreen(1920, 1080)
+test.setBackground(255, 255, 255)
 
-print(doAdd(5, 7))
+#clock = pygame.time.Clock()
 
-#This comment is new
+#img = gDraggable.draggable(0, 0, "default_texture.png")
+
+chess = gChessBoard.chess_board(test)
+
+        
+QUIT = False
+
+
+while not QUIT:
+        for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                        if event.key == K_ESCAPE:
+                                QUIT = True
+        chess.isClicked()
+        test.drawBackground()
+        chess.drawBoardandPieces(test.screen)
+        chess.isDropped()
+        chess.movePiece()
+        chess.generateValidMoves()
+        chess.checkMove()
+        #img.draw(test.screen)
+        #img.moveSprite()
+        test.update()
+        #clock.tick(60)
+
+print("Program closed!")
+test.quit()
+sys.exit()
